af486598150149fd76296f29e62f055cbec9e6db
Gayoung Kim <sogoodpiano@gmail.com>
2024-09-10 20:39:33 +0900

    Update app.js to use ejs and ejs-express-layout

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use("/", require("./routes/main"));
 
