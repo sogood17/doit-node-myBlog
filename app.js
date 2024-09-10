c972fec89f268b5f7bdd8ca4c1b80e13e18dbba9
Gayoung Kim <sogoodpiano@gmail.com>
2024-09-10 16:18:41 +0900

    🔨 Add router

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-})
-
 app.listen(port, () => {
   console.log("Server is running at "+port);
 })
