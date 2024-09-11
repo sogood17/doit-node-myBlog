c2951e7e3c94ca86b644280b35bc1a17793b4f74
Gayoung Kim <sogoodpiano@gmail.com>
2024-09-11 22:09:36 +0900

    connect database

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDb();
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
