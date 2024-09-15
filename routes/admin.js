f01409071f4f803904c098d1913108d6c42c3280
Gayoung Kim <sogoodpiano@gmail.com>
2024-09-15 16:55:28 +0900

    set login layout

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express= require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const adminLayout = "../views/layouts/admin.ejs";
+const loginLayout = "../views/layouts/admin.ejs";
 
 router.get("/admin", asyncHandler((req, res)=> {
-  res.render("admin", { layout: adminLayout })
+  const locals = {title : "admin"};
+  res.render("admin", { locals, layout: loginLayout })
 }));
 
 module.exports= router;
